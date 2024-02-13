d87bbd5eacbd91131fbfa0aaa04c99621b1db66d
melvinjariwala <melvinjariwala7.MJ@gmail.com>
2024-02-13 22:01:31 +0530

    Unit Tests

--- src/app/services/user-state.service.spec.ts
+++ src/app/services/user-state.service.spec.ts
@@ -6,11 +6,31 @@ describe('UserStateService', () => {
   let service: UserStateService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [UserStateService],
+    });
     service = TestBed.inject(UserStateService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should have initial user value as null', () => {
+    service.getUser().subscribe((user) => {
+      expect(user).toBeNull();
+    });
+  });
+
+  it('should set and get user', () => {
+    const mockUser = { id: 1, name: 'John Doe' };
+
+    // Set user
+    service.setUser(mockUser);
+
+    // Get user and check if it matches the mock user
+    service.getUser().subscribe((user) => {
+      expect(user).toEqual(mockUser);
+    });
   });
 });
